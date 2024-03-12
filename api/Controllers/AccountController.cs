8a39a8e6e815537596de13c0ec0ab08298e3b696
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-03-13 06:29:20 +0700

    update: add seeder role

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Account;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+	[Route("api/account")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+    private readonly UserManager<AppUser> _userManager;
+		public AccountController(UserManager<AppUser> userManager)
+		{
+      _userManager = userManager;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterDto registerDto){
+			try{
+				if(!ModelState.IsValid){
+					return BadRequest(ModelState);
+				}
+
+				var appUser = new AppUser{
+					UserName = registerDto.Username,
+					Email = registerDto.Email
+				};
+
+				var createUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+				if(createUser.Succeeded){
+					var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+					if(roleResult.Succeeded){
+						return Ok("User created");
+					}else{
+						return StatusCode(500, roleResult.Errors);
+					}
+				}else{
+					return StatusCode(500, createUser);
+				}
+			}catch(Exception e){
+				return StatusCode(500, e)
+			}
+		}
+	}
+}
