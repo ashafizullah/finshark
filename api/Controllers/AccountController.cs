28918d086c856ffc049a6b162abddd1d35925c5d
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-03-17 17:07:37 +0700

    update: login function

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -16,10 +17,33 @@ namespace api.Controllers
 	{
     private readonly UserManager<AppUser> _userManager;
 		private readonly ITokenService _tokenService;
-		public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+		private readonly SignInManager<AppUser> _signInManager;
+		public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
 		{
       _userManager = userManager;
 			_tokenService = tokenService;
+			_signInManager = signInManager;
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> Login(LoginDto loginDto){
+			if(!ModelState.IsValid) return BadRequest(ModelState);
+
+			var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+			if(user == null) return Unauthorized("Invalid username");
+
+			var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+			if(!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+
+			return Ok(
+				new NewUserDto{
+					UserName = user.UserName,
+					Email = user.Email,
+					Token = _tokenService.CreateToken(user)
+				}
+			);
 		}
 
 		[HttpPost("register")]
